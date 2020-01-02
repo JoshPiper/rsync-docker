b8bb523573451af32f782c49cfaa2249bc05fd13
John Internet <jonjon1234.github@gmail.com>
2020-01-02 14:49:21 +0000

    space -> tabs

--- agent-start.sh
+++ agent-start.sh
@@ -2,6 +2,6 @@
 
 # Start the SSH agent if it isn't already.
 if [ -z "$SSH_AGENT_PID" ]; then
-  eval $(ssh-agent) > /dev/null
-  echo "$SSH_AGENT_PID" > /tmp/agent-id
+	eval $(ssh-agent) > /dev/null
+	echo "$SSH_AGENT_PID" > /tmp/ssh-agent-id
 fi
